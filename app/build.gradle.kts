b5f87f230aed0eb54375496972b2aac46ec957b0
Gabriel <72566626+autowert66@users.noreply.github.com>
2026-09-15 09:30:43 +0200

    Bump version to 0.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "dev.a99.wifikill"
         minSdk = 26
         targetSdk = 34
-        versionCode = 8
-        versionName = "0.3.1"
+        versionCode = 9
+        versionName = "0.3.2"
     }
 
     signingConfigs {
